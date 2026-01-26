5a0a3ebbcc7b1a95dba3c6ccd9fa026a84fa8a20
Weskley Reis <weskleyff88004216@gmail.com>
2026-01-26 18:53:58 -0300

    Teste de integração do repository com Drizzle ORM - create e delete

--- src/core/todo/repositories/drizzle-todo.repository.test.ts
+++ src/core/todo/repositories/drizzle-todo.repository.test.ts
@@ -33,16 +33,91 @@ describe('DeizzleTodoRepository (integration)', () => {
   })
 
   describe('create', () => {
-    test('cria um todo se os dados estão válidos', async () => {})
+    test('cria um todo se os dados estão válidos', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
+      const newTodo = await repository.create(todos[0])
+      expect(newTodo).toStrictEqual({
+        success: true,
+        todo: todos[0],
+      })
+    })
 
-    test('falha se houver uma descrição igual na tabela', async () => {})
+    test('falha se houver uma descrição igual na tabela', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
 
-    test('falha se houver um ID igual na tabela', async () => {})
+      // Cria um novo todo
+      await repository.create(todos[0])
+
+      // Tenta criar um outro todo com a mesma descrição
+      const anotherTodo = {
+        id: 'any id',
+        description: todos[0].description,
+        createdAt: 'any date'
+      }
+      const result = await repository.create(anotherTodo)
+
+      expect(result).toStrictEqual({
+        success: false,
+        errors: ['Já existe um todo com o ID ou descrição enviados'],
+      })
+    })
+
+    test('falha se houver um ID igual na tabela', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
+
+      // Cria um novo todo
+      await repository.create(todos[0])
+
+      // Tenta criar um outro todo com o mesmo ID
+      const anotherTodo = {
+        id: todos[0].id,
+        description: 'any description',
+        createdAt: 'any date'
+      }
+      const result = await repository.create(anotherTodo)
+
+      expect(result).toStrictEqual({
+        success: false,
+        errors: ['Já existe um todo com o ID ou descrição enviados'],
+      })
+    })
+
+    test('falha se houver um ID e Descrição iguais', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
+      await repository.create(todos[0])
+      const anotherTodo = {
+        id: todos[0].id,
+        description: todos[0].description,
+        createdAt: 'any date'
+      }
+      const result = await repository.create(anotherTodo)
+
+      expect(result).toStrictEqual({
+        success: false,
+        errors: ['Já existe um todo com o ID e descrição enviados'],
+      })
+    })
   })
 
-  describe('delete', () => {
-    test('apaga um todo se ele existir', async () => {})
+  describe('remove', () => {
+    test('apaga um todo se ele existir', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
+      const result = await repository.remove(todos[0].id)
 
-    test('falha ao apagar se o todo não existir', async () => {})
+      expect(result).toStrictEqual({
+        success: true,
+        todo: todos[0],
+      })
+    })
+
+    test('falha ao apagar se o todo não existir', async () => {
+      const { repository, todos } = await makeTestTodoRepository()
+      const result = await repository.remove('any id')
+
+      expect(result).toStrictEqual({
+        success: false,
+        errors: ['Todo não existe'],
+      })
+    })
   })
 })
