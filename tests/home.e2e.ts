d6b46e37b53daf45b6141a1734146cfc07fa45ef
Weskley Reis <weskleyff88004216@gmail.com>
2026-05-07 07:48:46 -0300

    Testes E2E para criação com form, input e button usando Playwright no Next.js

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -39,14 +39,66 @@ test.describe('<Home /> (E2E)', () => {
       await expect(page).toHaveTitle('Testes com Vitest e Playwright')
     })
 
-    test('deve renderizar o cabeçalho, o input e o botão  para criar TODOs', async ({ page }) => {
+    test('deve renderizar o cabeçalho, o input e o botão  para criar TODOs',
+      async ({ page }) => {
       await expect(getHeading(page)).toBeVisible()
       await expect(getInput(page)).toBeVisible()
       await expect(getBtn(page)).toBeVisible()
     })
   })
 
-  // Criação
-  // Exclusão
-  // Erros
+  test.describe('Criação', () => {
+    test('deve permitir criar um TODO', async ({ page }) => {
+      const {btn, input} = getAll(page)
+
+      await input.fill(NEW_TODO_TEXT)
+      await btn.click()
+
+      const createTodo = page
+        .getByRole('listitem')
+        .filter({hasText: NEW_TODO_TEXT})
+      await expect(createTodo).toBeVisible()
+    })
+
+    test('deve fazer o trim da descrição do input ao criar o TODO',
+      async ({ page }) => {
+      const {btn, input} = getAll(page)
+
+      const textToBeTrimmed = '   no spaces here   '
+      const textTrimmed = textToBeTrimmed.trim()
+
+      await input.fill(textToBeTrimmed)
+      await btn.click()
+
+      const createdTodo = page
+        .getByRole('listitem')
+        .filter({ hasText: textTrimmed })
+      const createdTodoText = await createdTodo.textContent()
+
+      await expect(createdTodoText).toBe(textTrimmed)
+    })
+
+    test('deve permitir que eu crie mais de um TODO', async ({ page }) => {
+      const {btn, input} = getAll(page)
+
+      const todo1 = 'Todo 1'
+      const todo2 = 'Todo 2'
+
+      await input.fill(todo1)
+      await btn.click()
+
+      const todo1Item = page
+        .getByRole('listitem')
+        .filter({ hasText: todo1 })
+      await expect(todo1Item).toBeVisible()
+
+      await input.fill(todo2)
+      await btn.click()
+
+      const todo2Item = page
+        .getByRole('listitem')
+        .filter({ hasText: todo2 })
+      await expect(todo2Item).toBeVisible()
+    })
+  })
 })
